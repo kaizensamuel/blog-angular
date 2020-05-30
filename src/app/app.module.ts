f6e6b4f564bc86abddb3df67c2ae5027b0c659b6
Samuel Alarcon Cuadros <samuelac@correo.ugr.es>
2020-05-30 13:31:09 +0200

    #4 Formulario html insertado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 import { appRoutingProviders, routing } from './app.routing';
 import { LoginComponent } from './components/login/login.component';
